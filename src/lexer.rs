5db6763d4a934de4215b5cffdfb65efdfd3c55ff
Mustafa S. Moiz <msmoiz@amazon.com>
2023-07-17 16:27:24 -0700

    feat: add quoted value support

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::error::Result;
+use crate::{error::Result, parser::Quotes};
 
 #[derive(Debug, PartialEq)]
 pub enum Token {
@@ -11,14 +11,16 @@ pub struct Lexer<'a> {
     text: &'a str,
     pos: usize,
     delimiter: u8,
+    quotes: Quotes,
 }
 
 impl<'a> Lexer<'a> {
-    pub fn new(text: &'a str, delimiter: u8) -> Self {
+    pub fn new(text: &'a str, delimiter: u8, quotes: Quotes) -> Self {
         Self {
             text,
             pos: 0,
             delimiter,
+            quotes,
         }
     }
 
@@ -37,6 +39,15 @@ impl<'a> Lexer<'a> {
         if let Some(_) = self.scan_newline() {
             self.pos += 1;
             return Ok(Some(Newline));
+        }
+
+        if matches!(self.quotes, Quotes::Significant) {
+            if let Some(len) = self.scan_quoted_value() {
+                let value = &self.text[self.pos + 1..self.pos + 1 + len];
+                let value = value.replace(r#""""#, r#"""#);
+                self.pos += len + 2;
+                return Ok(Some(Value(value.into())));
+            }
         }
 
         let len = self.scan_value();
@@ -72,6 +83,31 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn scan_quoted_value(&self) -> Option<usize> {
+        assert!(self.pos < self.text.len());
+        let bytes = self.text.as_bytes();
+        let current = bytes[self.pos];
+        if current != b'"' {
+            return None;
+        }
+        let mut ix = self.pos + 1;
+        let mut len = 0;
+        while ix < bytes.len() {
+            if bytes[ix] == b'"' {
+                if ix + 1 < bytes.len() && bytes[ix + 1] == b'"' {
+                    ix += 2;
+                    len += 2;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            ix += 1;
+            len += 1;
+        }
+        Some(len)
+    }
+
     fn scan_value(&self) -> usize {
         assert!(self.pos < self.text.len());
         let bytes = self.text.as_bytes();
@@ -92,7 +128,7 @@ mod tests {
     #[test]
     fn peek() -> Result<()> {
         let text = ",";
-        let mut lexer = Lexer::new(text, b',');
+        let mut lexer = Lexer::new(text, b',', Quotes::Significant);
         assert_eq!(lexer.peek()?, Some(Delimiter));
         assert_eq!(lexer.peek()?, Some(Delimiter));
         Ok(())
@@ -101,14 +137,14 @@ mod tests {
     #[test]
     fn delim() {
         let text = ",";
-        let token = Lexer::new(text, b',').next().unwrap();
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
         assert_eq!(token, Some(Delimiter));
     }
 
     #[test]
     fn consecutive_delim() -> Result<()> {
         let text = ",,";
-        let mut lexer = Lexer::new(text, b',');
+        let mut lexer = Lexer::new(text, b',', Quotes::Significant);
         assert_eq!(lexer.next()?, Some(Delimiter));
         assert_eq!(lexer.next()?, Some(Delimiter));
         Ok(())
@@ -117,35 +153,35 @@ mod tests {
     #[test]
     fn newline() {
         let text = "\n";
-        let token = Lexer::new(text, b',').next().unwrap();
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
         assert_eq!(token, Some(Newline));
     }
 
     #[test]
     fn empty() {
         let text = "";
-        let token = Lexer::new(text, b',').next().unwrap();
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
         assert_eq!(token, None);
     }
 
     #[test]
     fn value() {
         let text = "foo";
-        let token = Lexer::new(text, b',').next().unwrap();
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
         assert_eq!(token, Some(Value("foo".into())));
     }
 
     #[test]
     fn value_with_spaces() {
         let text = "foo bar";
-        let token = Lexer::new(text, b',').next().unwrap();
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
         assert_eq!(token, Some(Value("foo bar".into())));
     }
 
     #[test]
     fn whitespace_values() -> Result<()> {
         let text = " , ";
-        let mut lexer = Lexer::new(text, b',');
+        let mut lexer = Lexer::new(text, b',', Quotes::Significant);
         assert_eq!(lexer.next()?, Some(Value(" ".into())));
         assert_eq!(lexer.next()?, Some(Delimiter));
         assert_eq!(lexer.next()?, Some(Value(" ".into())));
@@ -155,10 +191,58 @@ mod tests {
     #[test]
     fn delim_values() -> Result<()> {
         let text = "foo,bar";
-        let mut lexer = Lexer::new(text, b',');
+        let mut lexer = Lexer::new(text, b',', Quotes::Significant);
         assert_eq!(lexer.next()?, Some(Value("foo".into())));
         assert_eq!(lexer.next()?, Some(Delimiter));
         assert_eq!(lexer.next()?, Some(Value("bar".into())));
         Ok(())
+    }
+
+    #[test]
+    fn quoted_values() -> Result<()> {
+        let text = r#""foo","bar""#;
+        let mut lexer = Lexer::new(text, b',', Quotes::Significant);
+        assert_eq!(lexer.next()?, Some(Value("foo".into())));
+        assert_eq!(lexer.next()?, Some(Delimiter));
+        assert_eq!(lexer.next()?, Some(Value("bar".into())));
+        Ok(())
+    }
+
+    #[test]
+    fn quoted_value_with_newline() {
+        let text = "\"foo\nbar\"";
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
+        assert_eq!(token, Some(Value("foo\nbar".into())));
+    }
+
+    #[test]
+    fn quoted_value_with_delim() {
+        let text = r#""foo,bar""#;
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
+        assert_eq!(token, Some(Value("foo,bar".into())));
+    }
+
+    #[test]
+    fn quoted_value_insignificant() -> Result<()> {
+        let text = r#""foo,bar""#;
+        let mut lexer = Lexer::new(text, b',', Quotes::Insignificant);
+        assert_eq!(lexer.next()?, Some(Value("\"foo".into())));
+        assert_eq!(lexer.next()?, Some(Delimiter));
+        assert_eq!(lexer.next()?, Some(Value("bar\"".into())));
+        Ok(())
+    }
+
+    #[test]
+    fn escaped_quote() {
+        let text = r#""foo""""#;
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
+        assert_eq!(token, Some(Value("foo\"".into())));
+    }
+
+    #[test]
+    fn consecutive_escaped_quotes() {
+        let text = r#""foo""""""""#;
+        let token = Lexer::new(text, b',', Quotes::Significant).next().unwrap();
+        assert_eq!(token, Some(Value("foo\"\"\"".into())));
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -2,17 +2,33 @@ use crate::error::{Error, Result};
 use crate::lexer::Token;
 use crate::{lexer::Lexer, Dsv};
 
+/// Treatment of double quotes.
+pub enum Quotes {
+    /// Quotes have no special meaning and are considered part of the value.
+    Insignificant,
+    /// Quotes can be used to escape delimiters and newlines and are not
+    /// considered part of the value.
+    Significant,
+}
+
 /// Options for parsing a DSV file.
 pub struct Options {
     /// The delimiter that separates fields.
     ///
     /// Default: `,`.
     delimiter: u8,
+    /// Treatment of double quotes.
+    ///
+    /// Default: `Quotes::Significant`.
+    quotes: Quotes,
 }
 
 impl Default for Options {
     fn default() -> Self {
-        Self { delimiter: b',' }
+        Self {
+            delimiter: b',',
+            quotes: Quotes::Significant,
+        }
     }
 }
 
@@ -26,7 +42,7 @@ impl<'a> Parser<'a> {
     }
 
     pub fn from_str_with_options(text: &'a str, options: Options) -> Result<Dsv> {
-        let lexer = Lexer::new(text, options.delimiter);
+        let lexer = Lexer::new(text, options.delimiter, options.quotes);
         let mut parser = Self { lexer };
         parser.dsv()
     }
@@ -142,7 +158,14 @@ mod tests {
     #[test]
     fn tab_delim() {
         let text = "foo\tbar";
-        let dsv = Parser::from_str_with_options(text, Options { delimiter: b'\t' }).unwrap();
+        let dsv = Parser::from_str_with_options(
+            text,
+            Options {
+                delimiter: b'\t',
+                ..Default::default()
+            },
+        )
+        .unwrap();
         let mut expected = Dsv::new();
         expected.records.push(vec!["foo".into(), "bar".into()]);
         assert_eq!(dsv, expected);
